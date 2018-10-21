b7d7780a1e0f8ea00db7fb9dc7e8dbf2a885d907
wuguoxun <william.wu487@gmail.com>
2018-10-21 14:33:14 -0700

    cross screen

--- in-class_exercise_1/sketch.js
+++ in-class_exercise_1/sketch.js
@@ -1,28 +1,21 @@
+let x=200;
 // declare variables here
 function setup() {
-	createCanvas(500, 500);
-	// background is very light grey
-	background(230);
+  createCanvas(500, 500);
+
+  // set background color in grayscale:
+  background(150);
+
+  // draw ellipse
 }
 
 function draw() {
-
-	// create bright green ellipse with white stroke
-	fill(0, 255, 0);
-	stroke(255, 255, 255);
-	ellipse(100, 100, 80, 80);
-
-	// make transparent rectangle
-	// fourth argument will define transparency
-	// 125 is 50% transparency (half of 255)
-	fill(255, 100, 50, 127);
-	noStroke();
-	rect(100, 100, 80, 80);
-
-	// stroke color and transparency changes with mouse position
-	stroke(100, 200, mouseY, mouseX);
-	strokeWeight(5);
-	line(100, 100, mouseX, mouseY);
+  //
+background(0);
+	ellipse(x, 100, 80, 80);
+	x=x+1;
+	if (x>width+20)
+  {
+    x=-20;
+  }
 }
-
-// write custom functions here
