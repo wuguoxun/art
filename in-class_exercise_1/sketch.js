8942f5ba84b0b1b9e31093b898992084a223a1af
wuguoxun <william.wu487@gmail.com>
2018-10-29 13:54:16 -0700

    one page

--- in-class_exercise_1/sketch.js
+++ in-class_exercise_1/sketch.js
@@ -6,13 +6,22 @@ function setup() {
   // set background color in grayscale:
   background(150);
 
+	fill(0, 0, 255);
+
+	  noStroke();
+	  fill(100);
+	  rect(255,0,255);
+
+	  stroke(255, 0, 0);
+	  strokeWeight(10);
+	  rect(200, 200, 20)
   // draw ellipse
 }
 
 function draw() {
   //
-background(0);
-	ellipse(x, 100, 80, 80);
+background(1);
+	rect(x, 200, 80, 80);
 	x=x+1;
 	if (x>width+20)
   {
